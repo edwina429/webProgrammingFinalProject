d8475dc590a4e8f5f92ac3177035934ebcf387b0
edwin <e@mac.mynetworksettings.com>
2024-12-23 18:06:26 -0500

    removed deprecated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import { MdEmail } from "react-icons/md";
 import { TbPigMoney } from "react-icons/tb";
 import logo from './logo.svg';
 import Homepage from './components/homepage.js'; 
-import GoalsPage from './components/index.js'; 
 import Full_vault from './components/full_vault';
 import './App.css';
 
